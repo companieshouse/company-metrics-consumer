a7a9cd6a0fc6d0f8060888620218ec0be9b7faa4
sthompsonCH <sthompson@companieshouse.gov.uk>
2023-12-04 11:46:09 +0000

    DSND-2231: Delete temp java files

--- src/main/java/uk/gov/companieshouse/company/metrics/SonarQube.java
+++
@@ -1,8 +0,0 @@
-package uk.gov.companieshouse.company.metrics;
-
-public class SonarQube {
-    public int calculate(int first, int second) {
-        final int result = first + second;
-        return result;
-    }
-}
