0e75d946952d31dd71be74b4a637c680aad4e68c
crowlandsCH <crowlands@companieshouse.gov.uk>
2024-09-03 17:08:40 +0100

    DSND-2990: Initial implementation and fix unit tests.
    * Only 400 and 409 errors will produce nonRetryableErrors in MetricsApiResponseHandler now.
    * Any error related to the two above still log as error, the others at info.
    * Unit tests fixed

--- src/test/java/uk/gov/companieshouse/company/metrics/service/MetricsApiResponseHandlerTest.java
+++ src/test/java/uk/gov/companieshouse/company/metrics/service/MetricsApiResponseHandlerTest.java
@@ -16,12 +16,14 @@ import uk.gov.companieshouse.api.error.ApiErrorResponseException;
 import uk.gov.companieshouse.api.handler.exception.URIValidationException;
 import uk.gov.companieshouse.company.metrics.exception.NonRetryableErrorException;
 import uk.gov.companieshouse.company.metrics.exception.RetryableErrorException;
+import java.util.Arrays;
 
 @ExtendWith(MockitoExtension.class)
 class MetricsApiResponseHandlerTest {
 
     private static final String FAILED_MSG = "Failed recalculating %s for company %s";
-    private static final String ERROR_MSG = "Error %s recalculating %s for company %s";
+    private static final String ERROR_MSG = "HTTP response code %s  when recalculating %s for company %s";
+    private static final String API_INFO_RESPONSE_MSG = "Call to API failed, status code: %d. %s";
     private static final String COMPANY_NUMBER = "12345678";
     private static final String APPOINTMENTS_DELTA_TYPE = "appointments";
 
@@ -81,11 +83,12 @@ class MetricsApiResponseHandlerTest {
     void testHandleApiErrorResponseExceptionWhenStatusCode500() {
         // given
         final int statusCodeInternalServerError = 500;
-        String message = String.format(ERROR_MSG, statusCodeInternalServerError, APPOINTMENTS_DELTA_TYPE, COMPANY_NUMBER);
         HttpResponseException.Builder builder = new HttpResponseException.Builder(statusCodeInternalServerError, "", new HttpHeaders());
+        ApiErrorResponseException exception = new ApiErrorResponseException(builder);
+        String message = String.format(API_INFO_RESPONSE_MSG, statusCodeInternalServerError, Arrays.toString(exception.getStackTrace()));
 
         // when
-        Executable actual = () -> metricsApiResponseHandler.handle(COMPANY_NUMBER, APPOINTMENTS_DELTA_TYPE, new ApiErrorResponseException(builder));
+        Executable actual = () -> metricsApiResponseHandler.handle(COMPANY_NUMBER, APPOINTMENTS_DELTA_TYPE, exception);
 
         // then
         Exception ex = assertThrows(RetryableErrorException.class, actual);
@@ -93,9 +96,25 @@ class MetricsApiResponseHandlerTest {
     }
 
     @Test
-    void testHandleApiErrorResponseExceptionWhenStatusCode404() {
+    void testHandleApiErrorResponseExceptionWhenStatusCodeIsNot400Or409() {
         // given
-        final int statusCodeNotFound = 404;
+        final int statusCodeForbidden = 403;
+        HttpResponseException.Builder builder = new HttpResponseException.Builder(statusCodeForbidden, "", new HttpHeaders());
+        ApiErrorResponseException exception = new ApiErrorResponseException(builder);
+        String message = String.format(API_INFO_RESPONSE_MSG, statusCodeForbidden, Arrays.toString(exception.getStackTrace()));
+
+        // when
+        Executable actual = () -> metricsApiResponseHandler.handle(COMPANY_NUMBER, APPOINTMENTS_DELTA_TYPE, exception);
+
+        // then
+        Exception ex = assertThrows(RetryableErrorException.class, actual);
+        assertEquals(message, ex.getMessage());
+    }
+
+    @Test
+    void testHandleApiErrorResponseExceptionWhenStatusCode409() {
+        // given
+        final int statusCodeNotFound = 409;
         String message = String.format(ERROR_MSG, statusCodeNotFound, APPOINTMENTS_DELTA_TYPE, COMPANY_NUMBER);
         HttpResponseException.Builder builder = new HttpResponseException.Builder(statusCodeNotFound, "", new HttpHeaders());
 
@@ -108,11 +127,11 @@ class MetricsApiResponseHandlerTest {
     }
 
     @Test
-    void testHandleApiErrorResponseExceptionWhenStatusCodeIsNot404Or5xx() {
+    void testHandleApiErrorResponseExceptionWhenStatusCode400() {
         // given
-        final int statusCodeForbidden = 403;
-        String message = String.format(ERROR_MSG, statusCodeForbidden, APPOINTMENTS_DELTA_TYPE, COMPANY_NUMBER);
-        HttpResponseException.Builder builder = new HttpResponseException.Builder(statusCodeForbidden, "", new HttpHeaders());
+        final int statusCodeNotFound = 400;
+        String message = String.format(ERROR_MSG, statusCodeNotFound, APPOINTMENTS_DELTA_TYPE, COMPANY_NUMBER);
+        HttpResponseException.Builder builder = new HttpResponseException.Builder(statusCodeNotFound, "", new HttpHeaders());
 
         // when
         Executable actual = () -> metricsApiResponseHandler.handle(COMPANY_NUMBER, APPOINTMENTS_DELTA_TYPE, new ApiErrorResponseException(builder));
