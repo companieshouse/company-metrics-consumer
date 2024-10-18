090e2961ce1550e402e2812fcc6e8a267b7c98b8
sijegbulem-ch <sijegbulem@companieshouse.gov.uk>
2024-10-18 12:05:11 +0100

    empty pr

--- terraform/groups/ecs-service/locals.tf
+++ terraform/groups/ecs-service/locals.tf
@@ -54,7 +54,6 @@ locals {
       { "name"  = "${replace(upper(local.service_name), "-", "_")}_${var.ssm_version_prefix}${replace(upper(basename(sec.name)), "-", "_")}", "value" = sec.version }
   ]
 
-
   # secrets to go in list
   task_secrets = concat(local.global_secret_list,local.service_secret_list)
 
