551b49735115af99f78b3e1c60d736eecfb55858
iahmed-ch <iahmed@companieshouse.gov.uk>
2025-03-17 16:06:34 +0000

    set root read only file system to true

--- terraform/groups/ecs-service/main.tf
+++ terraform/groups/ecs-service/main.tf
@@ -30,7 +30,7 @@ module "secrets" {
 module "ecs-service" {
   source = "git@github.com:companieshouse/terraform-modules//aws/ecs/ecs-service?ref=1.0.300"
 
-  read_only_root_filesystem = false
+
 
   # Environmental configuration
   environment             = var.environment
