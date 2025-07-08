4bbf3fd52bbc88848046b8d2f4bbed49b89891c7
dhawes-ch <dhawes@companieshouse.gov.uk>
2025-07-08 07:11:44 +0100

    Revert f2ac151, ce1373d

--- src/itest/java/uk/gov/companieshouse/company/metrics/config/KafkaTestContainerConfig.java
+++ src/itest/java/uk/gov/companieshouse/company/metrics/config/KafkaTestContainerConfig.java
@@ -1,5 +1,11 @@
 package uk.gov.companieshouse.company.metrics.config;
 
+import static java.time.temporal.ChronoUnit.SECONDS;
+
+import java.time.Duration;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
 import org.apache.kafka.clients.producer.ProducerConfig;
@@ -8,11 +14,15 @@ import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
-import org.springframework.kafka.core.*;
+import org.springframework.kafka.core.ConsumerFactory;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
 import org.springframework.kafka.listener.ContainerProperties;
 import org.springframework.kafka.support.serializer.ErrorHandlingDeserializer;
+import org.testcontainers.containers.KafkaContainer;
 import org.testcontainers.containers.wait.strategy.Wait;
-import org.testcontainers.kafka.KafkaContainer;
 import org.testcontainers.utility.DockerImageName;
 import uk.gov.companieshouse.company.metrics.consumer.KafkaMessageConsumerAspect;
 import uk.gov.companieshouse.company.metrics.consumer.ResettableCountDownLatch;
@@ -21,13 +31,6 @@ import uk.gov.companieshouse.company.metrics.serialization.ResourceChangedDataDe
 import uk.gov.companieshouse.company.metrics.serialization.ResourceChangedDataSerializer;
 import uk.gov.companieshouse.company.metrics.steps.TestSupport;
 import uk.gov.companieshouse.stream.ResourceChangedData;
-
-import java.time.Duration;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
-import static java.time.temporal.ChronoUnit.SECONDS;
 
 @TestConfiguration
 public class KafkaTestContainerConfig {
@@ -50,7 +53,7 @@ public class KafkaTestContainerConfig {
 
     @Bean
     public KafkaContainer kafkaContainer() {
-        KafkaContainer kafkaContainer = new KafkaContainer(DockerImageName.parse("bitnami/kafka:latest").asCompatibleSubstituteFor("apache/kafka"));
+        KafkaContainer kafkaContainer = new KafkaContainer(DockerImageName.parse("confluentinc/cp-kafka:latest"));
         kafkaContainer.setWaitStrategy(Wait.defaultWaitStrategy()
                 .withStartupTimeout(Duration.of(300, SECONDS)));
         kafkaContainer.start();
